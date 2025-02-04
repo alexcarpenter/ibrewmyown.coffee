4630e0c29daf63483dc417e65ceaca96b193403a
Alex Carpenter <im.alexcarpenter@gmail.com>
2025-02-04 08:05:41 -0500

    redeploy

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
-// export const isProd = true;
 export const isProd = process.env.VERCEL_ENV === "production";
+// export const isProd = true;
 
 export function getSlugFromId(id: string) {
   return id.replace(/^\d{4}[-/.]\d{2}[-/.]\d{2}[-]/, "");
