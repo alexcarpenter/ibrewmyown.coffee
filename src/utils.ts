052a749f13722dbedc096c1792961a2f0841e044
Alex Carpenter <im.alexcarpenter@gmail.com>
2026-02-26 12:21:47 -0500

    v3

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
 export const isProd = process.env.VERCEL_ENV === "production";
-// export const isProd = true;
 
 export function getSlugFromId(id: string) {
   return id.replace(/^\d{4}[-/.]\d{2}[-/.]\d{2}[-]/, "");
@@ -9,9 +8,6 @@ export function getIndexById<T extends { id: number | string }>(
   arr: T[],
   id: number | string,
 ): string {
-  const index = arr
-    .slice()
-    .reverse()
-    .findIndex((item) => item.id === id);
+  const index = arr.slice().reverse().findIndex((item) => item.id === id);
   return (index + 1).toString().padStart(3, "0");
 }
